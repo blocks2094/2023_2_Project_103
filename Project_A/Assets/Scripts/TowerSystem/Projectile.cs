8ca6337f3c133f6ed4b2e40bbd36fdc24a5d98b7
blocks2094 <blocks2094@g.shingu.ac.kr>
2023-10-12 11:25:18 +0900

    Feat: "타워 Target 발사 까지"

---
+++ Project_A/Assets/Scripts/TowerSystem/Projectile.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody rd;        //내부에서 사용할 rd
+
+    public float moveSpped;     //이동속도
+    public float damagedAmount;     //데미지 량
+    private bool hasDamaged;        //flag 선언
+
+    
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+        rd.velocity = transform.forward * moveSpped;        //rd의 앞쪽 방향으로 총알 이동속도를 입력
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy" && !hasDamaged)
+        {
+            hasDamaged = true;
+        }
+
+        Destroy(gameObject);        //Trigger 충돌이 일어나면 파괴
+    }
+}
